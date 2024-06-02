275277226f312a56374e63169c87edb5dda4ac08
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-02 21:34:53 +0200

    working on new task submission form

--- src/components/NewTaskModal.jsx
+++ src/components/NewTaskModal.jsx
@@ -1,37 +1,56 @@
+import { useState } from "react";
+
 // this function will be responsible for creating a new task
 // creating a new task means assigning a new task name, decription type of list, due date
 
-function CreateNewTask() {
+function handleNewInput() {}
+
+function handleDropdownChange(e, setDropdown) {
+  console.log(e.target.value);
+  setDropdown(e.target.value);
+}
+
+function CreateNewTask(props) {
+  const [select, setSelect] = useState("");
+
   return (
     <div className="createNewTask">
       <div className="taskName">
         <label>Task Title</label>
-        <input type="text" />
+        <input
+          type="text"
+          className="nameInput"
+          onChange={(e) => console.log(e.target.className)}
+        />
       </div>
 
       <div className="taskDesc">
         <label>Task Description</label>
-        <textarea />
+        <textarea className="detailsInput" />
       </div>
 
       <div className="dueDate">
         <label>Due date</label>
-        <input type="date" />
+        <input type="date" className="dateInput" />
       </div>
 
       <div className="taskPrio">
         <label>Task Prio</label>
-        <select onChange={console.log("test")}>
-          <option>-- Choose Priority --</option>
-          <option>Low</option>
-          <option>Medium</option>
-          <option>High</option>
+        <select
+          className="prioInput"
+          value={select}
+          onChange={(e) => handleDropdownChange(e, setSelect)}
+        >
+          <option value="0">-- Choose Priority --</option>
+          <option value="1">Low</option>
+          <option value="2">Medium</option>
+          <option value="3">High</option>
         </select>
       </div>
 
       <div className="newTaskButtons">
-        <button>Save task</button>
-        <button>Cancel</button>
+        <button className="saveNewTask">Save task</button>
+        <button className="cancelNewTask">Cancel</button>
       </div>
     </div>
   );
