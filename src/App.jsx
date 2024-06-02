dc87a55e868efa7c72dc083e8df039bc4ed4b06f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-06-02 22:02:50 +0200

    Working on setting the temp task

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import { ToDoItems } from "./components/ToDoDetails";
 import { CreateNewTask } from "./components/NewTaskModal";
+import { createContext, useContext } from "react";
 import "./App.css";
 
+export const TaskContext = createContext();
+
 function App() {
-  const [newTask, setNewTask] = useState(false);
+  const [tempTask, setTempTask] = useState({
+    taskName: "",
+    taskDesc: "",
+    taskDate: "",
+    taskPrio: "",
+  });
   const [currentTasks, setCurrentTasks] = useState([]);
   return (
     <>
       <h1>To do app</h1>
       <ToDoItems />
-      <CreateNewTask />
+      <TaskContext.Provider value={{ tempTask, setTempTask }}>
+        <CreateNewTask />
+      </TaskContext.Provider>
     </>
   );
 }
--- src/components/NewTaskModal.jsx
+++ src/components/NewTaskModal.jsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { TaskContext } from "../App";
 
 // this function will be responsible for creating a new task
 // creating a new task means assigning a new task name, decription type of list, due date
+// i was going to use context API initially to avoid prop drilling
 
-function handleNewInput() {}
+// on change handlers for every input. Seperated into individual functions to follows Single Responsibility Principle
+// tempTask, setTempTask <- names of original tempState state
+
+function handleNewInputName(e, state) {
+  state.setTempTask({ ...state.tempTask, taskName: e.target.value });
+  console.log(e.target.value);
+  console.log(state.tempTask);
+}
+
+function handleNewInputDesc(e, state) {
+  console.log(e.target.className);
+  console.log(state.tempTask);
+}
+
+function handleNewInputDate(e, state) {
+  console.log(e.target.className);
+  console.log(state.tempTask);
+}
 
 function handleDropdownChange(e, setDropdown) {
   console.log(e.target.value);
   setDropdown(e.target.value);
 }
 
-function CreateNewTask(props) {
+function CreateNewTask() {
   const [select, setSelect] = useState("");
+  const tempState = useContext(TaskContext);
 
   return (
     <div className="createNewTask">
@@ -20,18 +40,25 @@ function CreateNewTask(props) {
         <input
           type="text"
           className="nameInput"
-          onChange={(e) => console.log(e.target.className)}
+          onChange={(e) => handleNewInputName(e, tempState)}
         />
       </div>
 
       <div className="taskDesc">
         <label>Task Description</label>
-        <textarea className="detailsInput" />
+        <textarea
+          className="detailsInput"
+          onChange={(e) => handleNewInputDesc(e, tempState)}
+        />
       </div>
 
       <div className="dueDate">
         <label>Due date</label>
-        <input type="date" className="dateInput" />
+        <input
+          type="date"
+          className="dateInput"
+          onChange={(e) => handleNewInputDate(e, tempState)}
+        />
       </div>
 
       <div className="taskPrio">
@@ -39,7 +66,7 @@ function CreateNewTask(props) {
         <select
           className="prioInput"
           value={select}
-          onChange={(e) => handleDropdownChange(e, setSelect)}
+          onChange={(e) => handleDropdownChange(e, setSelect, tempState)}
         >
           <option value="0">-- Choose Priority --</option>
           <option value="1">Low</option>
